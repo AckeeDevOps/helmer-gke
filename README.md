d47ba42bbd6779c3f5b87c1613065977686023a5
Stepan Vrany <vrany.stepan@gmail.com>
2019-01-20 20:17:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Helmer: a dogsbody for Helm deployment on GKE
+
+This simple image is meant to be executed in CI/CD environments 
+in the grand finale when you want to deploy your Helm chart to GKE.
+
+It does not aim to be an universal tool to solve all the misery 
+of this universe. It just makes one very specific scenario happen.
+
+## Gitlab CI example
+
+```yaml
+deploy:gke:
+  image: ackee/helmer-gke
+  environment: production
+  variables:
+    # GCLOUD_SA_KEY: this one should be defined under Settings > CI/CD > Environment variables
+    # you can even interpolate different variables for different environments:
+    # GCLOUD_SA_KEY: $GCLOUD_SA_KEY_PRODUCTION
+    GCLOUD_PROJECT_ID: my-project-12345
+    GCLOUD_GKE_CLUSTER_NAME: production
+    GCLOUD_GKE_ZONE: europe-west3-c
+  scripts:
+    - gke-init
+    # optional step, check https://github.com/vranystepan/vaultier/
+    - vaultier
+    - helm upgrade release-name -i -f /path/to/values.yaml -f /path/to/secrets /path/to/chart
+```
---
+++ gke-init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Required variables:
+#   GCLOUD_SA_KEY: base64 encoded string with service account key (cat /pat/to/key | base64 -w0)
+#   GCLOUD_PROJECT_ID: gcloud project ID in plain text e.g. 'my-project-12345'
+#   GCLOUD_GKE_CLUSTER_NAME: your GKE cluster name in plain text e.g. 'production'
+#   GCLOUD_GKE_ZONE: GCP zone where your GKE cluster is running e.g. 'europe-west3-c'
+
+# Check required variables
+[ -z "$GCLOUD_SA_KEY" ] && { echo "GCLOUD_SA_KEY is required"; exit 1; }
+[ -z "$GCLOUD_PROJECT_ID" ] && { echo "GCLOUD_PROJECT_ID is required"; exit 1; }
+[ -z "$GCLOUD_GKE_CLUSTER_NAME" ] && { echo "GCLOUD_GKE_CLUSTER_NAME is required"; exit 1; }
+[ -z "$GCLOUD_GKE_ZONE" ] && { echo "GCLOUD_GKE_ZONE is required"; exit 1; }
+
+echo "Initializing gcloud auth"
+echo GCLOUD_SA_KEY | base64 -d > /tmp/key.json
+gcloud auth activate-service-account --key=/tmp/key.json
+
+echo "Initializing GKE cluster"
+gcloud container cluster get-credentials ${GCLOUD_GKE_CLUSTER_NAME} && \
+  --zone=${GCLOUD_GKE_ZONE} && \
+  --project=${GCLOUD_PROJECT_ID}
+
