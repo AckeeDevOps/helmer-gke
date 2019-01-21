a27f2d0fd017999867d20e87b7a33ed42f37aa06
Stepan Vrany <stepan.vrany@ackee.cz>
2019-01-21 09:01:30 +0100

    correct gcloud flag

--- gke-init.sh
+++ gke-init.sh
@@ -14,7 +14,7 @@
 
 echo "Initializing gcloud auth"
 echo GCLOUD_SA_KEY | base64 -d > /tmp/key.json
-gcloud auth activate-service-account --key=/tmp/key.json
+gcloud auth activate-service-account --key-file=/tmp/key.json
 
 echo "Initializing GKE cluster"
 gcloud container cluster get-credentials ${GCLOUD_GKE_CLUSTER_NAME} && \
